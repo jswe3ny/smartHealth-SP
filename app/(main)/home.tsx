2462bfbfe909ae609e937f4b6073b2c525ed77e3
Jack Sweeny <jsweeny344@gmail.com>
2025-09-23 16:04:54 -0700

    Revert "Food journal page / Different feature modals / Minor Bugs  (#3)"
    This reverts commit dd80272236138d2dd3a32be4b992a7bc6541ae5f, reversing
    changes made to f5609888d5955bab77da947cc8590e40fd0bcd19.

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/button";
 import { GoalContainer } from "@/components/GoalContainer";
 import { useAuth } from "@/contexts/authContext";
 import { useUserInfo } from "@/hooks/useUserInfo";
-import { router } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
@@ -11,11 +10,6 @@ export default function Home() {
   const userData = useUserInfo();
   console.log("client: ", userData.profile?.currentGoals);
   if (!currentUser) return;
-
-   const navigateToFoodJournal = () => {
-    router.push("/(tracking)/foodJournal");
-  };
-
   return (
     <View>
       <Text>Home</Text>
@@ -23,14 +17,6 @@ export default function Home() {
       {/* ################### TESTING Firebase Operations ####################### */}
       {/* <TestFormsNative uid={userData.profile?.docId!} /> */}
       {/* ################### TESTING Operations  ####################### */}
-
-      <Button
-        title="Food Journal"
-        onPress={navigateToFoodJournal}
-        size="lg"
-        bg="#4CAF50"
-      />
-
       <GoalContainer
         goals={userData.profile?.currentGoals}
         id={currentUser?.uid}
--- app/(tracking)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function TrackingLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="foodJournal" options={{ title: "Food Journal" }} />
-    </Stack>
-  );
-}
